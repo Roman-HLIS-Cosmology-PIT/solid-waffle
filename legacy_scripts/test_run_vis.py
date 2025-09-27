c1542c5b22caf85a0308bcaaca793228fbbe7ca3
hirata10 <hirata.10@osu.edu>
2025-09-27 17:56:41 -0400

    another pyproject update

--- legacy_scripts/test_run_vis.py
+++ legacy_scripts/test_run_vis.py
@@ -2,4 +2,4 @@ import sys
 
 from solid_waffle.correlation_run import run_vis_all
 
-run_vis_all(sys.argv[1], run_ir_first = bool(len(sys.argv)>=3))
+run_vis_all(sys.argv[1], run_ir_first=bool(len(sys.argv) >= 3))
