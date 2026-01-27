6770f994d3a0c2f63af0190c95089b85644956e4
doublea1084@gmail.com <doublea1084@gmail.com>
2026-01-27 16:46:53 -0500

    testing getting asdf files for new converter

---
+++ src/asdf_to_fits.py
@@ -0,0 +1,15 @@
+import numpy as np
+import asdf
+import fitsio
+import glob
+from astropy.io import fits
+import time
+
+
+asdf_files = glob.glob(os.path.join(os.getcwd(), "*.asdf"))
+print("ASDF files found: ")
+for file in asdf_files:
+    print(file)
+
+    
+
