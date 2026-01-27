65262bf684ccacd9d6ffb93dca2487f2057dc2ca
doublea1084@gmail.com <doublea1084@gmail.com>
2026-01-27 18:57:18 -0500

    added converting to fits files for testing

--- src/solid_waffle/asdf_to_fits.py
+++ src/solid_waffle/asdf_to_fits.py
@@ -6,11 +6,22 @@ import glob
 from astropy.io import fits
 import time
 
+def main():
+    current_directory = os.getcwd();
+    asdf_files = glob.glob(os.path.join(current_directory, "*.asdf"))
+    print("ASDF files found: ") 
+    for fn in asdf_files:
+        print(file)
+    for fn in asdf_files:
+        new_file_path = os.path.join(output_dir, base.replace(".asdf", "asdf_to.fits"))
+        if os.path.exists(new_file_path):
+            print(f"Skipping (already exists): {new_file_path}")
+            continue
+        with asdf.open(fn) as f:
+            fits.PrimaryHDU(f["roman"]["data"].value.astype(np.uint16)).writeto(new_file_path, overwrite=True)
 
-asdf_files = glob.glob(os.path.join(os.getcwd(), "*.asdf"))
-print("ASDF files found: ")
-for file in asdf_files:
-    print(file)
 
+if __name__ == "__main__":
+    main()
     
 
