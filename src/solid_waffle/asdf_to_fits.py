c936e6b12747cc94b274fc5e44d65676c93a1893
doublea1084@gmail.com <doublea1084@gmail.com>
2026-01-27 19:06:04 -0500

    fixed

--- src/solid_waffle/asdf_to_fits.py
+++ src/solid_waffle/asdf_to_fits.py
@@ -9,13 +9,14 @@ import time
 def main():
     current_dir = os.getcwd();
     output_dir = os.path.join(os.path.dirname(current_dir), "fits_converted")
+    os.makedirs(output_dir, exist_ok=True)
     asdf_files = glob.glob(os.path.join(current_dir, "*.asdf"))
     print("ASDF files found: ") 
     for fn in asdf_files:
         print(fn)
     for fn in asdf_files:
         base = os.path.basename(fn)
-        new_file_path = os.path.join(output_dir, base.replace(".asdf", "asdf_to.fits"))
+        new_file_path = os.path.join(output_dir, base.replace(".asdf", "_asdf_to.fits"))
         if os.path.exists(new_file_path):
             print(f"Skipping (already exists): {new_file_path}")
             continue
