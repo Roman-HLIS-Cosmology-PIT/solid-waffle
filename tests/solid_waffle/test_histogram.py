782390d2508e94e7b49e953146f28a7e4fe3a990
hirata10 <hirata.10@osu.edu>
2026-02-21 21:00:05 -0500

    ruff reformat

--- tests/solid_waffle/test_histogram.py
+++ tests/solid_waffle/test_histogram.py
@@ -31,11 +31,11 @@ def test_histogram(tmp_path):
 
     # make the data cubes
     stamp = 20260221
-    for i in range(1,N+1):
+    for i in range(1, N + 1):
         data = np.zeros((nread, 4096, 4096), dtype=np.uint16)
         for j in range(nread):
-            a = 1. + (x + y) / 8192.0
-            sig = 2000. + 0.5 * y + 1000. * j + a * (20.0 * np.cos(x / 100.) + 15.0 * np.sin(y / 10.0))
+            a = 1.0 + (x + y) / 8192.0
+            sig = 2000.0 + 0.5 * y + 1000.0 * j + a * (20.0 * np.cos(x / 100.0) + 15.0 * np.sin(y / 10.0))
             sfrac, _ = np.modf(sig / 128.0)
             sig -= np.where(sfrac < 0.01, sfrac, 0.01) * 128
             data[j, :, :] = np.clip(np.round(sig + 0.501), 0, 65535)
@@ -44,10 +44,15 @@ def test_histogram(tmp_path):
             stamp += 1
 
     outfile = str(tmp_path) + "/hist.txt"
-    histograms_main([None, "-f", "1", "-i", str(tmp_path) + "/20260220T_001.fits", "-o", outfile, "-n", str(N)])
+    histograms_main(
+        [None, "-f", "1", "-i", str(tmp_path) + "/20260220T_001.fits", "-o", outfile, "-n", str(N)]
+    )
     data = np.loadtxt(outfile).astype(np.int32)
-    diff = data[2555:2566, 2] - np.array([1028, 1076, 884, 1016, 660, 0, 2308, 1068, 992, 1188, 1160], dtype=np.int32)
+    diff = data[2555:2566, 2] - np.array(
+        [1028, 1076, 884, 1016, 660, 0, 2308, 1068, 992, 1188, 1160], dtype=np.int32
+    )
     assert np.all(np.abs(diff) <= 1)
+
 
 def test_histogram2(tmp_path):
     """
@@ -74,11 +79,11 @@ def test_histogram2(tmp_path):
     del _a
 
     # make the data cubes
-    for i in range(1,N+1):
+    for i in range(1, N + 1):
         data = np.zeros((nread, 4096, 4096), dtype=np.uint16)
         for j in range(nread):
-            a = 1. + (x + y) / 8192.0
-            sig = 2000. + 0.5 * y + 1000. * j + a * (20.0 * np.cos(x / 100.) + 15.0 * np.sin(y / 10.0))
+            a = 1.0 + (x + y) / 8192.0
+            sig = 2000.0 + 0.5 * y + 1000.0 * j + a * (20.0 * np.cos(x / 100.0) + 15.0 * np.sin(y / 10.0))
             sfrac, _ = np.modf(sig / 128.0)
             sig -= np.where(sfrac < 0.01, sfrac, 0.01) * 128
             data[j, :, :] = np.clip(np.round(sig + 0.501), 0, 65535)
@@ -87,5 +92,7 @@ def test_histogram2(tmp_path):
     outfile = str(tmp_path) + "/hist.txt"
     histograms_main([None, "-f", "1", "-i", str(tmp_path) + "/myfile_001.fits", "-o", outfile, "-n", str(N)])
     data = np.loadtxt(outfile).astype(np.int32)
-    diff = data[2555:2566, 2] - np.array([1028, 1076, 884, 1016, 660, 0, 2308, 1068, 992, 1188, 1160], dtype=np.int32)
+    diff = data[2555:2566, 2] - np.array(
+        [1028, 1076, 884, 1016, 660, 0, 2308, 1068, 992, 1188, 1160], dtype=np.int32
+    )
     assert np.all(np.abs(diff) <= 1)
